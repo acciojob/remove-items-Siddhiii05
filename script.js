822523097a96aec2c40fd829e002536d4995b92c
Siddhiii05 <siddhi.bobade05@gmail.com>
2026-06-27 17:12:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function removeColour() {
+	const select = document.getElementById("colorSelect");
+	const index = select.selectedIndex;
+	
+	if(index !== -1){
+		select.remove(index)
+	}
+}
